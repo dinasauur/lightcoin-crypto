5707e868396b26694746188a88d8d0d8a692e712
Wendina <wendina.teng@gmail.com>
2023-03-20 06:58:01 +0000

    Create a deposit and account class. Passed the account object reference to withdrawal/deposit when creating the w/d. Pattern called dependency injection that means passing an object the things it needs rather than having the object access them itself. Something is wrong with the code, even though it's almost exactly the same as Compass.

--- index.js
+++ index.js
@@ -1,29 +1,65 @@
-let balance = 500.00;
+// let balance = 500;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username,
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+}
 
-  constructor(amount) {
+class Deposit {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
   }
 
 }
 
+class Withdrawal {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  // new method learned: commit method is called in order to finalize and apply that transaction to the account's balance.
+  commit() {
+    this.account.balance -= this.amount;
+  }
+}
 
 
-
+// Instantiate the withdrawal and deposit
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol')
+console.log('Starting Account Balance: ', myAccount.balance);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+
 
 t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(150);
+t3.commit();
+console.log(`Transaction 3: `, t3);
+console.log(`Balance: ${t3.account.balance}`);
+
+/****
+* Note - Here we are passing the myAccount object reference to the withdrawal/deposit object when we create it.
+*This process of passing an object the information it needs when we create it is a great design pattern. *
+* It means that, unlike before, the Withdrawal and Deposit objects are not dependent on any surrounding data in their global or outer scope.
+* Additionally, transactions are no longer tied to only a single account. We can have these transaction records work with any account.
+* This pattern is called Dependency Injection. It's a fancy word that simply means "passing an object the things it needs rather than having the object access them itself".
+* It makes for code that is much more modular and testable.
+****/
